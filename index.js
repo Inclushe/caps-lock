af4d14e9a7145ed8fe473b4d9dae6f516639f549
Ethan <inclushe@gmail.com>
2018-07-28 01:16:06 -0400

    Boilerplate and init repo

---
+++ index.js
@@ -0,0 +1,7 @@
+var app = require('express')()
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world!</h1>')
+})
+var server = app.listen(process.env.PORT, () => {
+  console.log(`Listening on localhost:${server.address().port}`)
+})
