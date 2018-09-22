17be30e392fa8d151d8f771ec043f6d33db15aba
Ethan <inclushe@gmail.com>
2018-09-22 01:06:01 -0400

    Changed the logo...again

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var app = require('./app')
-require('dotenv').config({path: './vars.env'})
+require('dotenv').config({ path: './vars.env' })
 var server = app.listen(process.env.PORT, () => {
   console.log(`Listening on ${server.address().port}`)
 })
